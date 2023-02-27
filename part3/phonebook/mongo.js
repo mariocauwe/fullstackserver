77d1b5c4e5f3cfeda5166141c691aa87a0876ce2
mariocauwe <mariocauwe@gmail.com>
2023-02-27 19:46:26 +0200

    ex 3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,57 @@
+
+//Shell: mongosh "mongodb+srv://cluster0.youqbtb.mongodb.net/myFirstDatabase" --apiVersion 1 --username openstack
+const mongoose = require('mongoose')
+
+console.log("command line parameters",process.argv.length)
+
+
+if (process.argv.length<3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const dbUrl = `mongodb+srv://openstack:${password}@cluster0.youqbtb.mongodb.net/?retryWrites=true&w=majority`
+
+const createPerson =  (name, number) => {
+    const newPerson = new Person({name:name, number:number})
+    newPerson.save().then(res => {
+        console.log("Person saved");
+        mongoose.connection.close()
+    })
+}
+
+const getAllPersons = () => {
+    Person.find({}).then(res => {
+        res.forEach(person => {
+            console.log(person);                
+        })
+        mongoose.disconnect() 
+    })
+}
+
+
+mongoose.set('strictQuery',false)
+mongoose.connect(dbUrl).then(res => {
+        console.log("connected to mongodb")     
+    }).catch(err => {
+        console.log("Failed to connect to mongodb",err)
+        process.exit(1)     
+    })
+
+    const personSchema = new mongoose.Schema
+    (   {
+            name: String,
+            number: String
+        } )
+    
+    const Person = mongoose.model('Person',personSchema)
+
+    if (process.argv.length===5) {
+        const name = process.argv[3]
+        const number = process.argv[4]
+        console.log("name",name,"number",number)
+        createPerson(name,number)
+    }
+    else getAllPersons()
+
