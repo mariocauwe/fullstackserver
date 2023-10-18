0da77f0063da71b2f4a765cf8fab122175c3d619
mariocauwe <mariocauwe@gmail.com>
2023-10-18 11:40:20 +0200

    4.5

--- part4/bloglist/tests/totallikes.test.js
+++ part4/bloglist/tests/totallikes.test.js
@@ -1,43 +1,63 @@
 const listHelper = require('../utils/list_helper')
+const logger = require('../utils/logger')
+
+const listWithOneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    }
+]
+const listWithMultiBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: '652ea054ee45d76f4715a877',
+        title: 'blog 2',
+        author: 'mario',
+        url: 'https://fullstackopen.com/',
+        likes: 12,
+        __v: 0
+    },
+    {
+        _id: '652fa371581d0924cd7e96c2',
+        title: 'blog 3',
+        author: 'Jimmy',
+        url: 'https://jestjs.io/',
+        likes: 7,
+        __v: 0
+    }
+]
 
 describe('total likes', () => {
-    const listWithOneBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-        }
-    ]
-
     test('when list has only one blog, equals the likes of that', () => {
         const result = listHelper.totalLikes(listWithOneBlog)
         expect(result).toBe(5)
     })
 
-    const listWithMultiBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-            likes: 5,
-            __v: 0
-        },
-        {
-            _id: '652ea054ee45d76f4715a877',
-            title: 'blog 2',
-            author: 'mario',
-            url: ' https://fullstackopen.com/',
-            likes: 12,
-            __v: 0
-        }
-    ]
-
     test('when list has only one blog, equals the likes of that', () => {
         const result = listHelper.totalLikes(listWithMultiBlog)
         expect(result).toBe(17)
     })
-  })
+    test('when list has no blogs, equals the likes of that', () => {
+        const result = listHelper.totalLikes([])
+        expect(result).toBe(0)
+    })
+})
+
+describe('favorite blog', () => {
+    test.only('find the first most favourite blog', () => {
+        const result = listHelper.favoriteBlog(listWithMultiBlog)
+        //logger.info('result in test',result)
+        expect(result).toEqual({ author: 'mario', title: 'blog 2', likes: 12 })
+    })
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,14 +1,32 @@
+const logger = require('../utils/logger')
 
 const dummy = (blogs) => {
+    logger.info('returning fixed 1 for',blogs)
     return 1
 }
 
 const totalLikes = (blogs) => {
+    if(blogs.length===0) return 0
     const reducer = (sum, blog) => {
         return sum + blog.likes
     }
     return blogs.reduce(reducer,0)
 }
+
+const favoriteBlog = (blogs) => {
+    if(blogs.length===0) return {}
+    let favorite=blogs[0]
+    blogs.map( (blog) => {
+        if(blog.likes>favorite.likes)
+            favorite = blog
+    })
+    return {
+        author: favorite.author,
+        title: favorite.title,
+        likes: favorite.likes
+    }
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }
